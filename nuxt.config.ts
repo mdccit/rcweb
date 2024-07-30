db75340b60dd3fb4d3ec4677ea9276f484c711de
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-07-30 15:20:14 +0530

    #135209 - installed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/eslint-module",
     "@nuxt/eslint",
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  plugins: [
+    '~/plugins/pinia'
+  ],
+
+  compatibilityDate: "2024-07-30",
 })
