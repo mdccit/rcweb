62f73e1cf10953cadcf09f3a3a025e67eeb92da9
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-08-06 17:56:59 +0530

    #135521 table create

---
+++ src/pages/admin/moderation.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <EPDataTable />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useUserStore } from '~/stores/userStore';
+  import  EPDataTable from '~/components/EPDataTable.vue';
+  
+  const userStore = useUserStore()
+  
+  const email = userStore.user?.email
+  const token = userStore.user?.token
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #333;
+  }
+  
+  p {
+    font-size: 16px;
+  }
+  </style>
+  
