7568d29d61cb8d7d463ef2679e980687f6a78285
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-08-21 17:50:42 +0530

    #135727 staff Modal change

--- src/pages/admin/schoolStaff.vue
+++ src/pages/admin/schoolStaff.vue
@@ -7,18 +7,16 @@
                         stroke-linejoin="round">
                         <path d="M15 6l-6 6l6 6"></path>
                     </svg></a>
-                <h2 class="font-bold text-lg self-center"> Editing: SchoolAdm1 </h2>
+                <h2 class="font-bold text-lg self-center text-black"> Editing: SchoolAdm1 </h2>
             </div>
-            <div class=""><a
-                    href="https://qa1.recruited.qualitapps.com/app/school/9c2845cc-7676-45e1-b498-13f930b22e9b"><button
-                        type="submit"
-                        class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-700 border-gray-300 focus:border-primary-300 focus:ring-primary-200">
-                        View <svg class="w-5 h-5 -mr-1 inline" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M17 7l-10 10"></path>
-                            <path d="M8 7l9 0l0 9"></path>
-                        </svg></button></a></div>
+            <div class=""><button type="submit"
+                    class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-700 border-gray-300 focus:border-primary-300 focus:ring-primary-200">
+                    View <svg class="w-5 h-5 -mr-1 inline" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M17 7l-10 10"></path>
+                        <path d="M8 7l9 0l0 9"></path>
+                    </svg></button></div>
         </div>
     </div>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -40,10 +38,10 @@
             <div class="my-8"></div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
                 <div class="flex justify-between">
-                    <div class="flex-1 text-2xl font-bold mb-4"> All Staff Members </div>
+                    <div class="flex-1 text-2xl font-bold mb-4 text-black"> All Staff Members </div>
                     <div class="">
                         <button type="submit"
-                            class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-blue-500 hover:bg-primary-400 active:bg-primary-600 text-white border-transparent focus:border-primary-300 focus:ring-primary-200">
+                            class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-blue-500 hover:bg-blue-700 active:bg-primary-600 text-white border-transparent focus:border-primary-300 focus:ring-primary-200">
                             Add User
                         </button>
 
@@ -51,16 +49,16 @@
                 </div>
                 <div class="grid gap-4">
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -73,16 +71,16 @@
                     </div>
 
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -95,16 +93,16 @@
                     </div>
 
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -118,7 +116,7 @@
                 </div>
             </div>
             <div class="my-8"></div>
-          
+
         </div>
 
         <div class="">
@@ -128,24 +126,24 @@
                     <div class="flex-1 text-2xl font-bold mb-4"> Team name </div>
                     <div class="">
                         <button type="submit"
-                            class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-blue-500 hover:bg-primary-400 active:bg-primary-600 text-white border-transparent focus:border-primary-300 focus:ring-primary-200">
-                           Create Team
+                            class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-blue-500 hover:bg-blue-700 active:bg-primary-600 text-white border-transparent focus:border-primary-300 focus:ring-primary-200">
+                            Create Team
                         </button>
 
                     </div>
                 </div>
                 <div class="grid gap-4">
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -158,16 +156,16 @@
                     </div>
 
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -180,16 +178,16 @@
                     </div>
 
                     <div class="flex flex-col lg:flex-row gap-6 py-2">
-                            <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
-                                class="flex flex-row gap-4 flex-1"><img
-                                    src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
-                                    alt="User profile picture" class="h-12 w-12 rounded-full">
-                               
-                                    <div class="self-center flex-1"><strong class="capitalize">PlayerAdm1</strong>
-                                    <p class="opacity-75 line-clamp-1">Role title: Coach</p>
-                                </div>
-                            </a>
-                       
+                        <a href="https://qa1.recruited.qualitapps.com/app/u/9c28453b-5cc0-4c0a-b9bf-9c6b95b44a70"
+                            class="flex flex-row gap-4 flex-1"><img
+                                src="https://ui-avatars.com/api/?name=P&amp;color=7F9CF5&amp;background=EBF4FF"
+                                alt="User profile picture" class="h-12 w-12 rounded-full">
+
+                            <div class="self-center flex-1 text-black"><strong class="capitalize">PlayerAdm1</strong>
+                                <p class="opacity-75 line-clamp-1 text-gray">Role title: Coach</p>
+                            </div>
+                        </a>
+
                         <div class="self-center flex flex-wrap gap-2">
                             <a
                                 href="https://qa1.recruited.qualitapps.com/admin/schools/9c2845cc-7676-45e1-b498-13f930b22e9b/users/189/edit">
@@ -203,7 +201,8 @@
                 </div>
             </div>
             <div class="my-8"></div>
-          
+
+
         </div>
     </div>
 </template>
