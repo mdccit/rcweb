eb49294be4e4b5a3a5dad81cef99e507d557bd4b
Madubha Amunugama <madubha.amunugama@qualitapps.com>
2024-08-22 08:59:18 +0530

    #135588 UI filter location card

--- src/pages/user/user-search.vue
+++ src/pages/user/user-search.vue
@@ -1,35 +1,12 @@
 <template>
     <div class="mt-16">
-    <div class="card rounded-2xl overflow-hidden border border-lightSteelBlue bg-white w-full p-6 mt-3">
-        <div class="flex justify-between items-center mb-4">
-            <h2 class="text-lg font-semibold">Location</h2>
-            <button class="flex text-ceil text-sm">
-                <svg class="w-3 h-3 mt-1 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                <span> Clear</span>
-            </button>
-        </div>
-        <div class="space-y-2">
-            <input type="text"
-                class="w-full px-3 py-2 border border-timberwolf rounded focus:outline-none focus:ring focus:border-graySnowDrift"
-                placeholder="Illinois">
-            <input type="text"
-                class="w-full px-3 py-2 border border-timberwolf rounded focus:outline-none focus:ring focus:border-graySnowDrift"
-                placeholder="City">
-        </div>
-    </div>
-    <!-- End Location Card -->
-
-    <!-- End Filter Section  -->
 
     <!-- Start Content Section -->
     <div class="col-span-5 sm:col-span-4 md:col-span-5 lg:col-span-3 xl:col-span-4">
-        <h2 class="text-lg font-semibold">Search Result</h2>
-        <label class="text-sm mb-2">Lorem ipsum is a placeholder text commonly used to</label>
-        <div class="flex flex-wrap gap-2">
+        <h2 class="text-lg font-semibold text-black">Search Result</h2>
+        <label class="text-sm mb-2  text-black">Lorem ipsum is a placeholder text commonly used to</label>
+
+        <div class="flex flex-wrap gap-2 my-4">
             <div class="flex items-center bg-steelBlue text-white px-3 py-1 rounded-md">
                 <span class="text-sm">Location</span>
                 <button class="ml-2 focus:outline-none" aria-label="Remove tag">
@@ -56,7 +33,7 @@
             <div class="col-span-2">
                 <div class="card rounded-2xl overflow-hidden border border-lightSteelBlue bg-white w-full p-6 mt-3">
                     <div class=" flex">
-                        <img class="w-40 h-30 rounded-md" src="../assets/css/images/rectangle-117.png"
+                        <img class="w-40 h-30 rounded-md" src="../../assets/user/images/Rectangle 117.png"
                             alt="Profile Picture">
                         <div class="ml-4 flex-1">
                             <div class="flex justify-between items-center">
