01b4fccad9f9169648f1dc26c8344fab494e0226
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-08-30 13:45:38 +0530

    #135623 coach profile tabs

--- src/pages/user/coachProfile.vue
+++ src/pages/user/coachProfile.vue
@@ -7,59 +7,88 @@
                 <img class="w-full h-[400px] rounded-xl" src="../../assets/user/images/coach-cover.jpg " alt="">
                 <span>
                     <div class="-mt-[144px]">
-                        <div class="col-span-5 sm:col-span-1 md:col-span-5 lg:col-span-1 xl:col-span-1">
+                        <div class="grid grid-cols-5 gap-4 flex">
 
-                            <div
-                                class="grid gap-4 grid-cols-5 sm:grid-cols-5 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5">
-                                <div class="text-center">
-                                    <img class="mx-auto w-[200px] h-[200px] rounded-xl"
+                            <div class="col-span-1">
+                                <div class="text-center flex">
+                                    <img class="mx-auto w-[150px] h-[150px] rounded-xl mt-[45px]"
                                         src="../../assets/user/images/coachProPic.png" alt="">
-                                </div>
-                                <div class="">
-                                    <div class="text-left mt-[70px] ">
+
+                                    <div class="text-left mt-[80px] ">
                                         <h2 class="text-lg font-semibold  text-white text-3xl">Jane Cooper</h2>
                                         <h5 class=" text-md  text-white font-normal text-black text-primaryblue">Tennis
                                             player
                                         </h5>
                                     </div>
                                 </div>
-                                <div class="">
+
+                            </div>
+
+                            <div class="col-span-3">
+                                <div
+                                    class="mt-[140px] text-sm font-medium text-center text-gray-500 border-b border-gray-200 text-gray-400 border-gray-400">
+                                    <ul class="flex flex-wrap -mb-px">
+                                        <li class="me-2">
+                                            <a href="#"
+                                                class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:text-blue-500 dark:border-blue-500">Post</a>
+                                        </li>
+                                        <li class="me-2">
+                                            <a href="#"
+                                                class="inline-block p-4 border-b-2 border-transparent rounded-t-lg active  hover:border-gray-300 dark:hover:text-gray-300"
+                                                aria-current="page">Connections</a>
+                                        </li>
+                                        <li class="me-2">
+                                            <a href="#"
+                                                class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Media</a>
+                                        </li>
+
+
+                                    </ul>
                                 </div>
-                                <div class="">
-                                </div>
-                                <div class="mt-[70px]">
-                                    <div>
-                                        <button type="button" class=" w-[45px] h-[45px] text-blue-500  focus:outline-none focus:ring-4 focus:ring-blue-300
-                             font-medium rounded-full text-sm text-center me-2 mb-2 bg-white
-                              hover:bg-blue-200 dark:focus:ring-blue-800 p-0">
+                            </div>
+                            <div class="col-span-1 mt-[70px]">
+                                <div>
+                                    <div class="flex">
+                                        <div class="">
+                                            <button class="bg-lighterGray rounded-full w-[35px] h-[35px] p-0 m-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor"
+                                                    class="size-5 text-blue-500 m-auto">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        <div class="">
+                                            <button class="bg-lighterGray rounded-full w-[35px] h-[35px] p-0 m-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor"
+                                                    class="size-5 text-blue-500 m-auto">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+                                                </svg>
 
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-6 mx-auto">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                                            </svg>
+                                            </button>
+                                        </div>
 
-                                        </button>
-                                        <button type="button" class=" w-[45px] h-[45px] text-blue-500  focus:outline-none focus:ring-4 focus:ring-blue-300
-                             font-medium rounded-full text-sm text-center me-2 mb-2 bg-white
-                              hover:bg-blue-200 dark:focus:ring-blue-800 p-0">
+                                        <div>
+                                            <button class="bg-blue-500 rounded-full  p-2 m-1 text-xs h-[35px] w-[85px]">
+                                                Connect +
+                                            </button>
+                                        </div>
+                                        <div class="">
+                                            <button class="bg-lighterGray rounded-full w-[35px] h-[35px] p-0 m-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor"
+                                                    class="size-5 text-blue-500 m-auto">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                </svg>
 
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-6 mx-auto">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-                                            </svg>
-
-
-                                        </button>
-                                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300
-                             font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600
-                              dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-normal">Connect+
-                                        </button>
-
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
 
                         </div>
@@ -91,7 +120,11 @@
                 <!-- End Profile pic section  -->
 
                 <!-- Start post Section -->
+
                 <div class="col-span-5 sm:col-span-3 md:col-span-5 lg:col-span-2 xl:col-span-3">
+
+
+
                     <!--start card 01 -->
                     <div class="card rounded-2xl overflow-hidden border border-lightSteelBlue bg-white w-full p-6">
                         <div class="flex items-center">
