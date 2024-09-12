8a878c6994c6535427d985324f53e03f1f8eeb1b
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-09-12 09:27:31 +0530

    #136417 nofification color change

--- src/components/common/Notification.vue
+++ src/components/common/Notification.vue
@@ -2,9 +2,9 @@
   <div v-if="visible" :class="notificationClasses" class="fixed top-4 right-4 w-96 border rounded-lg shadow-lg p-4">
     <div class="flex justify-between items-center">
       <div class="flex items-center">
-        <span :class="messageClasses">{{ message }}</span>
+        <span class="text-sm text-white" :class="messageClasses">{{ message }}</span>
       </div>
-      <button @click="closeNotification" class="text-gray-500 hover:text-gray-900">
+      <button @click="closeNotification" class="text-white">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
@@ -45,17 +45,17 @@ if (props.duration > 0) {
 // Computed classes for notification based on type
 const notificationClasses = computed(() => {
   return {
-    'bg-green-100 border-green-500': props.type === 'success',
-    'bg-red-100 border-red-500': props.type === 'failure',
-    'bg-yellow-100 border-yellow-500': props.type === 'warning',
+    'bg-limegreen border-green-500': props.type === 'success',
+    'bg-red-500 border-red-500': props.type === 'failure',
+    'bg-yellow-400 border-yellow-500': props.type === 'warning',
   };
 });
 
 const messageClasses = computed(() => {
   return {
-    'text-green-600': props.type === 'success',
-    'text-red-600': props.type === 'failure',
-    'text-yellow-600': props.type === 'warning',
+    'text-white': props.type === 'success',
+    'text-white': props.type === 'failure',
+    'text-white': props.type === 'warning',
   };
 });
 </script>
