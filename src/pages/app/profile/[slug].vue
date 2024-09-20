9fa8745fa0045a3fcb71c4fb3f4722d9636d940a
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-09-20 18:41:40 +0530

    #136617 - dynamically loading profile layout

--- src/pages/app/profile/[slug].vue
+++ src/pages/app/profile/[slug].vue
@@ -6,7 +6,7 @@
 <script setup>
 import { ref, onMounted, computed, defineAsyncComponent } from 'vue';
 import { useRoute } from 'vue-router';
-import { useNuxtApp } from '#app';
+import { useNuxtApp  } from '#app';
 
 const user = ref(null); // Initialize the user as null, will be an object later
 const loading = ref(true); // To show loading state
@@ -15,22 +15,40 @@ const error = ref(null); // To handle any potential errors
 const route = useRoute(); // Access route params and query
 const slug = route.params.slug;
 
-// Fetch user data using the id
+// Fetch user data using the slug
 const fetchUserData = async () => {
   const nuxtApp = useNuxtApp();
-  const $userService = nuxtApp.$publicService; // Assuming $adminService is your service
+  const $userService = nuxtApp.$publicService; // Assuming $userService is your service
 
   try {
     loading.value = true; // Start loading
     if (slug) {
       const response = await $userService.get_user_profile(slug);
-       user.value = response;
-      
-     
+      user.value = response;
     }
 
     if (!user.value) {
       throw new Error("User not found");
+    }
+
+    // Define layout dynamically based on user role ID after fetching user data
+    const roleId = user.value.user_basic_info?.user_role_id;
+    switch (roleId) {
+      case 4:
+        // Player layout
+        definePageMeta({ layout: 'profile-without-cover' });
+        break;
+      case 5:
+        // Coach layout
+        definePageMeta({ layout: 'profile-without-cover' });
+        break;
+      case 6:
+        // Business User layout
+        definePageMeta({ layout: 'profile-without-cover' });
+        break;
+      default:
+        // Fallback layout
+        definePageMeta({ layout: 'profile-without-cover' });
     }
 
   } catch (err) {
@@ -52,7 +70,7 @@ const profileComponent = computed(() => {
 
   const roleId = user.value.user_basic_info?.user_role_id;
 
-  switch (roleId) {    
+  switch (roleId) {
     case 4:
       // Load PlayerProfile component lazily
       return defineAsyncComponent(() => import('~/components/profiles/playerProfile.vue'));
