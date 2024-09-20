24f14e2de6c256d43b90667011245907ac975af5
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-09-20 11:19:45 +0530

    #136665  player profile ui fix

--- src/components/profiles/playerProfile.vue
+++ src/components/profiles/playerProfile.vue
@@ -5,10 +5,22 @@
                 <!-- Start Profile pic section  -->
                 <div class="col-span-5 sm:col-span-1 md:col-span-5 lg:col-span-1 xl:col-span-1">
 
-                    <div>
+                    <div class="group">
                         <div class="text-center">
-                            <img class="mx-auto w-[200px] h-[200px] rounded-xl"
-                                src="../../assets/user/images/Rectangle 193.png" alt="">
+                            <div class="w-[200px] h-[200px] relative">
+                                <img class="mx-auto w-[200px] h-[200px] rounded-xl"
+                                    src="../../assets/user/images/Rectangle 193.png" alt="">
+                                <div
+                                    class="absolute top-0 right-0 m-2 rounded-lg flex justify-end opacity-0 group-hover:opacity-100 transition-opacity duration-200 cursor-pointer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor"
+                                        class="w-6 h-6 bg-white p-[3px] rounded-md">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+                                </div>
+                            </div>
+
                             <h3 class="text-lg font-semibold text-center text-black">{{ name }}</h3>
                             <h5 class="text-normal text-md text-center text-black text-primaryblue">Tennis player</h5>
                         </div>
@@ -18,10 +30,17 @@
 
                     <div class=" card rounded-2xl overflow-hidden border border-lightSteelBlue bg-white p-3 mt-3">
                         <div class="flex items-center justify-between">
-
-                            <div class="flex items-center space-x-4 w-48">
-
-                                <h1 class="text-lg font-semibold mb-4 text-black">Bio</h1>
+                            <div class="flex items-center space-x-4 w-48 grid grid-cols-10">
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-8">Bio</h1>
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-2">
+                                    <div class="cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </div>
+                                </h1>
                             </div>
                         </div>
                         <p class="text-xs text-darkSlateBlue leading-relaxed mb-4">
@@ -31,10 +50,17 @@
 
                     <div class=" card rounded-2xl overflow-hidden border border-lightSteelBlue bg-white p-3 mt-3">
                         <div class="flex items-center justify-between">
-
-                            <div class="flex items-center space-x-4 w-48">
-
-                                <h1 class="text-lg font-semibold mb-4 text-black">Info</h1>
+                            <div class="flex items-center space-x-4 w-48 grid grid-cols-10">
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-8"></h1>
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-2">
+                                    <div class="cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </div>
+                                </h1>
                             </div>
                         </div>
                         <p class="text-xs text-darkSlateBlue leading-relaxed mb-4  ml-2"> <b>{{ city }} , {{ country
@@ -46,7 +72,7 @@
                             <b>{{ Number(pounds.toFixed(1)) }}lb ({{ weight }} kg)</b>
                         </p>
                         <p class="text-xs text-darkSlateBlue leading-relaxed mb-4  ml-2"> <b>Graduation {{
-                                graduationDate }}</b> </p>
+                            graduationDate }}</b> </p>
                         <p class="text-xs text-darkSlateBlue leading-relaxed mb-4  ml-2"> <b>{{ birthday }} Years Old
                             </b> </p>
                     </div>
