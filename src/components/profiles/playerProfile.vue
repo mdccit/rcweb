b9586c02f0c3993809fe030daab9bf55d3f1b72e
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-09-20 11:25:02 +0530

    #136665 player profile edit buttons

--- src/components/profiles/playerProfile.vue
+++ src/components/profiles/playerProfile.vue
@@ -476,6 +476,21 @@
                         </div>
                     </div>
                     <div class="card rounded-2xl overflow-hidden border border-lightSteelBlue bg-blue-500 p-3 mt-2">
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center space-x-4 w-48 grid grid-cols-10">
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-8"></h1>
+                                <h1 class="text-lg font-semibold mb-4 text-black col-span-2">
+                                    <div class="cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </div>
+                                </h1>
+                            </div>
+                        </div>
+
                         <div class="grid grid-cols-2 gap-4">
                             <div class="">
                                 <h2 class="text-lg font-semibold mb-4">UTR score</h2>
