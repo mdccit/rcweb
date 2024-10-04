95baaf71fb0d4beaf81c550d428b72d608a5e2ec
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-10-04 14:08:17 +0530

    #137112 - cache busted url added

--- src/components/profiles/coach/tabs/mediaTab.vue
+++ src/components/profiles/coach/tabs/mediaTab.vue
@@ -25,7 +25,7 @@
       <ul class="mt-4">
         <li v-for="(file, index) in files" :key="index" class="mb-2 flex justify-between items-center">
           <span>{{ file.name }} ({{ file.type }})</span>
-          <button @click.prevent="removeFile(index)"   class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700">
+          <button @click.prevent="removeFile(index)" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700">
             Remove
           </button>
         </li>
@@ -60,11 +60,18 @@
           Your browser does not support the video tag.
         </video>
         <button @click="removeMediaItem(item.media_id, $event)"
-          class="bg-red-500 text-white px-2 py-1 mt-2 rounded hover:bg-red-700" v-if="loggedUserSlug == props.userSlug"  :disabled="loadingStates[item.media_id]">
+          class="bg-red-500 text-white px-2 py-1 mt-2 rounded hover:bg-red-700" v-if="loggedUserSlug == props.userSlug"
+          :disabled="loadingStates[item.media_id]">
           Remove
-          <svg  v-if="loadingStates[item.media_id]" aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+          <svg v-if="loadingStates[item.media_id]" aria-hidden="true" role="status"
+            class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="#E5E7EB" />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentColor" />
           </svg>
         </button>
       </div>
@@ -114,6 +121,12 @@ const galleryItems = ref([]);
 const isImageCropping = ref(false); // Toggle for image cropping
 const cropperImage = ref(null); // Image to be cropped
 
+watch(() => props.userSlug, (newVal) => {
+  if (newVal && props.userSlug) {
+    fetchGalleryBySlug();
+  }
+});
+
 // Handle file selection
 const handleFileUpload = (event) => {
   const selected = Array.from(event.target.files);
@@ -145,7 +158,7 @@ const removeMediaItem = async (media_id, event) => {
   } catch (error) {
     loading.value = false;
     nuxtApp.$notification.triggerNotification(error.display_message || 'Failed to remove media', 'failure');
-  } finally{
+  } finally {
     loadingStates.value[media_id] = false;
   }
 };
@@ -171,12 +184,14 @@ const uploadMedia = async () => {
       files.value = [];
       loading.value = false;
 
+      files.value.forEach((file, index) => removeMediaItem(index));
       nuxtApp.$notification.triggerNotification(response.display_message, 'success');
+
+      // Emit the event after successful upload
+      emit('uploadCompleted');
       fetchGalleryBySlug();
 
-      files.value.forEach((file, index) => removeMediaItem(index));
-      // Emit the event after successful upload
-      emit('uploadMedia');
+
     } else {
       loading.value = false;
       nuxtApp.$notification.triggerNotification(response.display_message, 'warning');
@@ -204,35 +219,39 @@ const fetchGalleryBySlug = async () => {
     const dataSets = await $publicService.get_user_profile(props.userSlug);
 
     if (dataSets.media_info) {
-      setGalleryItems(dataSets.media_info);
+      setGalleryItems(dataSets.media_info); // Update the gallery items
     } else {
       console.log('No media info available');
     }
   } catch (error) {
-    console.log(error);
+    console.error('Error fetching data:', error.message);
   }
 };
 
 
 const setGalleryItems = (mediaInfo) => {
   galleryItems.value = mediaInfo.media_urls.map(media => {
+    // Append a timestamp to the URL to prevent browser caching
+    const cacheBustedUrl = `${media.url}?t=${new Date().getTime()}`;
+
     if (media.media_type === 'image') {
       return {
         type: 'image',
-        href: media.url,
-        src: media.url,
+        href: cacheBustedUrl,
+        src: cacheBustedUrl,
         media_id: media.media_id,
       };
     } else if (media.media_type === 'video') {
       return {
         type: 'video',
-        href: media.url,
-        src: media.url || 'https://via.placeholder.com/200x150.png?text=Video',
+        href: cacheBustedUrl,
+        src: cacheBustedUrl || 'https://via.placeholder.com/200x150.png?text=Video',
         media_id: media.media_id,
       };
     }
   });
 };
+
 
 onMounted(() => {
   Fancybox.bind('[data-fancybox="gallery"]', {
@@ -265,9 +284,8 @@ onMounted(() => {
   if (process.client) {
     loggedUserSlug.value = localStorage.getItem('user_slug');
 
-    if(props.userSlug) {
     fetchGalleryBySlug();
-    }
+
   }
 });
 </script>
