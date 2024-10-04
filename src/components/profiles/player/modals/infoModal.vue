7e711928c130a170798895567a3cea77de0f9c92
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-04 18:30:58 +0530

    #137101 player edit date of birth fix

--- src/components/profiles/player/modals/infoModal.vue
+++ src/components/profiles/player/modals/infoModal.vue
@@ -207,7 +207,8 @@
                                             <label for="graduation_month_year"
                                                 class="block mb-1 text-sm font-normal text-gray-900">Graduation
                                                 Month/Year</label>
-                                            <div class="flex rounded-lg border border-gray-300 shadow-sm rounded-[10px]">
+                                            <div
+                                                class="flex rounded-lg border border-gray-300 shadow-sm rounded-[10px]">
                                                 <input type="month" v-model="graduation_month_year" required
                                                     class="lock text-black px-5 py-3 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg" />
                                             </div>
@@ -224,8 +225,8 @@
                                 class="inline-flex w-full justify-center rounded-md bg-steelBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto">Save
                                 changes
                                 <svg v-if="loading" aria-hidden="true" role="status"
-                                    class="inline w-4 h-4 me-3 text-white animate-spin ml-2 mt-[3px]" viewBox="0 0 100 101"
-                                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    class="inline w-4 h-4 me-3 text-white animate-spin ml-2 mt-[3px]"
+                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                                         fill="#E5E7EB" />
@@ -307,6 +308,17 @@ onMounted(async () => {
 
     ]);
 });
+// Function to get today's date in 'YYYY-MM-DD' format
+const getTodayDate = () => {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = String(today.getMonth() + 1).padStart(2, '0')
+    const day = String(today.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
+}
+
+// Assign today's date to the max attribute
+const today = getTodayDate()
 
 watch(() => props.visible, (newVal) => {
     if (newVal && props.slug) {
