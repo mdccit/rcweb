fa5b62f847c0027dd11551e9bab90c6a66a8d054
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-07 15:37:38 +0530

    #137217 user transcripton ui

---
+++ src/pages/admin/userTranscription.vue
@@ -0,0 +1,78 @@
+<template>
+    <header class="bg-gray-200">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="flex w-full justify-between gap-8">
+                <div class="flex items-center gap-4">
+                    <NuxtLink to="/admin/users"><svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M15 6l-6 6l6 6"></path>
+                        </svg></NuxtLink>
+                    <h2 class="font-bold text-lg self-center text-black"> Editing: f d </h2>
+                </div>
+                <div class="">
+                    <NuxtLink to="/app/u/9c9ad039-6106-40d6-92de-b5c7556b05a7"><button type="submit"
+                            class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-700 border-gray-300 focus:border-primary-300 focus:ring-primary-200">
+                            View <svg class="w-5 h-5 -mr-1 inline" xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M17 7l-10 10"></path>
+                                <path d="M8 7l9 0l0 9"></path>
+                            </svg></button></NuxtLink>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-black">
+            <div class="flex gap-x-4">
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50">
+                        Account Details </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/player/profile"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50">
+                        Profile </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/player/media"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50"> Media
+                    </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/connections"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50">
+                        Connections </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/lists"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50"> Lists
+                    </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/danger-zone"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 opacity-50">
+                        Danger Zone </button></NuxtLink>
+                <NuxtLink to="/admin/users/9c9ad039-6106-40d6-92de-b5c7556b05a7/danger-zone"><button
+                        class="text-black px-4 py-2 rounded hover:bg-gray-200 transition duration-200 bg-gray-200">
+                        Transcript </button></NuxtLink>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useUserStore } from '~/stores/userStore'
+import AdminUserCreateModal from '~/components/admin/user/adminUserCreateModal.vue';
+import AdminUserLogoutSession from '~/components/admin/user/adminUserLogoutSession.vue';
+import AdminUserDelete from '~/components/admin/user/adminUserDelete.vue';
+import Notification from '~/components/common/Notification.vue';
+
+import { useRouter } from 'vue-router';
+definePageMeta({
+    ssr: false,
+    layout: 'admin',
+    middleware: ['role'],
+    requiredRole: ['admin'],
+});
+</script>
+
+<style scoped>
+.container {
+    max-width: 600px;
+}
+</style>
