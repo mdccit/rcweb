540e5a3e8b1c4277554e09b975bc35cc20c65a0b
Ashan R <ashan.rajapaksha@qualitapps.com>
2024-10-09 13:14:37 +0530

    #137204 - school api calls moved to public service

--- src/components/profiles/schoolProfile/modals/nameModal.vue
+++ src/components/profiles/schoolProfile/modals/nameModal.vue
@@ -2,7 +2,7 @@
   <!-- Name change modal -->
   <div v-if="visible" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-
+      <form @submit.prevent="saveProfilePicture">
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <div
@@ -10,25 +10,8 @@
                   <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                       <div class="sm:flex sm:items-start">
                           <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
-                              <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Change your
-                                  Name</h3>
+                              <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Change School Profile Picture</h3>
                               <div class="mt-2 w-full">
-                                  <!-- First Name Input -->
-                                  <div class="w-full">
-                                      <label class="block mb-1 text-gray-700 font-sans">School Name
-                                          <span aria-hidden="true" class="text-red-600"
-                                              title="This field is required">*</span>
-                                      </label>
-                                      <div class="flex rounded-lg border border-gray-300 shadow-sm w-full">
-                                          <input id="name" v-model="name" type="text"
-                                              autocomplete="name"
-                                              class="w-full block px-5 py-3  text-black border-0 focus:border-lightAzure focus:ring focus:ring-lightPastalBlue focus:ring-opacity-50 disabled:opacity-50 disabled:bg-gray-50 disabled:cursor-not-allowed rounded-lg"
-                                              placeholder="Enter your first name" required>
-                                      </div>
-                                      <InputError :error="errors.name ? errors.name.join(', ') : ''" />
-                                  </div>
-
-
                                   <!-- Profile Picture Upload -->
                                   <div class="w-full mt-3">
                                       <label class="block mb-1 text-gray-700 font-sans">Profile Picture
@@ -71,7 +54,7 @@
                       </div>
 
                       <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                          <button type="button" @click="saveName"
+                          <button type="submit"
                               class="inline-flex w-full justify-center rounded-md bg-steelBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto">Save
                               changes</button>
                           <button type="button" @click="$emit('close', 'name')"
@@ -81,6 +64,8 @@
               </div>
           </div>
       </div>
+      </form>
+
   </div>
 </template>
 
@@ -251,18 +236,7 @@ const updatePlayerNames = async (firstName, lastName, otherNames) => {
   }
 };
 
-// Save names when the user clicks "Save changes"
-const saveName = async () => {
 
-  try {
-      await saveProfilePicture();
-      await updatePlayerNames(first_name.value, last_name.value, other_names.value); // Call the API to update the player's names
-      emit('close', 'name'); // Emit close event after successfully updating the names
-  } catch (error) {
-      console.error('Error during save process:', error);
-  }
-
-};
 
 const removeProfile = async () => {
   try {
