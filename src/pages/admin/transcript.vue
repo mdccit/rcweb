96a160de6a5728f79f4a7756df616c4b7f3841d2
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-10 12:56:21 +0530

    #137217 transcript modal

--- src/pages/admin/transcript.vue
+++ src/pages/admin/transcript.vue
@@ -33,34 +33,34 @@
                 <el-input v-model="search" class="h-[40px] mr-2" placeholder="Search..." clearable></el-input>
 
                 <button type="button" aria-haspopup="true" id="dropdownButton" data-dropdown-toggle="dropdowntable"
-          class="text-white bg-gray-200 hover:bg-gray-300 focus:ring-4 p-2 border rounded h-[40px] w-[50px] mr-1 ">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 mx-auto" viewBox="0 0 20 20"
-            fill="currentColor">
-            <path fill-rule="evenodd"
-              d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-              clip-rule="evenodd"></path>
-          </svg>
-        </button>
-        <!-- Dropdown Menu -->
-        <div id="dropdowntable"
-          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10 hidden p-3">
-          <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                    class="text-white bg-gray-200 hover:bg-gray-300 focus:ring-4 p-2 border rounded h-[40px] w-[50px] mr-1 ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 mx-auto" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <!-- Dropdown Menu -->
+                <div id="dropdowntable"
+                    class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10 hidden p-3">
+                    <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
 
-            <div class="mb-3">
-              <label for="">Status </label>
-              <div class="flex  border border-gray-300 shadow-sm rounded-[10px]">
-                <select name="filter-role" v-model="role" @change="fetchData"
-                  class="lock text-black px-5 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg">
-                  <option value=""> - </option>
-                  <option value="2"> A.I.Ferified </option>
-                  <option value="5"> A.I.Faild </option>
-                  <option value="4"> Pending </option>
-                  <option value="6"> Manual </option>
-                </select>
-              </div>
-            </div>            
-          </div>
-        </div>
+                        <div class="mb-3">
+                            <label for="">Status </label>
+                            <div class="flex  border border-gray-300 shadow-sm rounded-[10px]">
+                                <select name="filter-role" v-model="role" @change="fetchData"
+                                    class="lock text-black px-5 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg">
+                                    <option value=""> - </option>
+                                    <option value="2"> A.I.Ferified </option>
+                                    <option value="5"> A.I.Faild </option>
+                                    <option value="4"> Pending </option>
+                                    <option value="6"> Manual </option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <!-- <button
         class="text-white bg-gray-200 hover:bg-gray-300 focus:ring-4 p-2 border rounded h-[40px] w-[50px] mr-1 mx-auto">
@@ -172,6 +172,65 @@
             </el-pagination>
         </el-card>
     </div>
+
+
+
+    <!-- status change modal -->
+<!-- 
+    <div v-if="visible" class="relative z-index-220" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div
+                    class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
+                                <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Change
+                                   State</h3>
+                                <div class="mt-2 w-full">   
+                        
+                                    <div class="w-full mt-3">
+                                        <label class="block mb-1 text-gray-700 font-sans"> Select State
+                                            <span aria-hidden="true" class="text-red-600"
+                                                title="This field is optional"></span>
+                                        </label>
+                                        <select
+                                            class="lock pt-1 text-black h-12 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg">
+                                            <option value="">Manual</option>
+                                        </select>
+                                       
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                        <button type="button"
+                            class="inline-flex w-full justify-center rounded-md bg-steelBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                            :disabled="loading">Save
+                            changes
+                            <svg v-if="loading" aria-hidden="true" role="status"
+                                class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="#E5E7EB" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentColor" />
+                            </svg></button>
+                        <button type="button"
+                            class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
 
 </template>
 
