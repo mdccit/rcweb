f0db432332d528041fc1414f0c87ffea56ab7d69
Isuru Sandalanka <isuru.sandalanka@qualitapps.com>
2024-10-10 10:47:31 +0530

    #137245 Show the menu items are clickable by changing the mouse pointer

--- src/components/user/navbar.vue
+++ src/components/user/navbar.vue
@@ -163,13 +163,13 @@
                                 aria-labelledby="dropdownUserAvatarButton">
                                 <li>
                                     <NuxtLink @click="gotoAdminDashboard"
-                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        class="cursor-pointer block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                         Admin Dashboard</NuxtLink>
                                 </li>
                             </ul>
                             <div class="py-2">
                                 <NuxtLink @click="logout"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                    class="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                                     Sign out</NuxtLink>
                             </div>
                         </div>
