a47db0e506035923f61572920739c254bd146c4f
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-10 18:42:21 +0530

    #137000 coach modal changes

--- src/components/profiles/coach/modals/coverModal.vue
+++ src/components/profiles/coach/modals/coverModal.vue
@@ -21,13 +21,16 @@
                       <div class="w-full flex justify-center">
 
                       </div>
-                      <button class="p-2 rounded-lg bg-red-600 mx-auto m-2 text-white" @click="removeCover"><svg
-                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                          stroke="currentColor" class="size-6">
-                          <path stroke-linecap="round" stroke-linejoin="round"
-                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                      </button>
+                      <div class="w-full flex justify-center">
+                        <button class="p-2 rounded-lg bg-red-600 mx-auto m-2 text-white" @click="removeCover"><svg
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                          </svg>
+                        </button>
+                      </div>
+
                     </div>
 
 
@@ -59,7 +62,7 @@
               class="inline-flex w-full justify-center rounded-md bg-steelBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 sm:ml-3 sm:w-auto"
               :disabled="loading">Save
               changes
-              <svg v-if="loading" aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin"
+              <svg v-if="loading" aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin ml-1 mt-1"
                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -110,10 +113,10 @@ onMounted(() => {
 });
 
 watch(() => props.visible, (newVal) => {
-    if (newVal && props.slug) {
-        fetchCoachNames(props.slug);
-        console.log(455)
-    }
+  if (newVal && props.slug) {
+    fetchCoachNames(props.slug);
+    console.log(455)
+  }
 });
 
 const fetchCoachNames = async (slug) => {
@@ -128,7 +131,7 @@ const fetchCoachNames = async (slug) => {
     //     other_names.value = dataSets.user_basic_info.other_names ?? "";
     // }
     console.log('media');
-    console.log(dataSets.media_info.cover_picture    )
+    console.log(dataSets.media_info.cover_picture)
     if (dataSets.media_info) {
       cover_picture.value = dataSets.media_info.cover_picture ?? null
 
