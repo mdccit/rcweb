1e91e435a988355eb5e393ae74e777026bc39dea
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-10 10:03:08 +0530

    #137353 profile ui fixes

--- src/components/profiles/businessUserProfile/modals/coverModal.vue
+++ src/components/profiles/businessUserProfile/modals/coverModal.vue
@@ -14,13 +14,51 @@
                 <div class="mt-2 w-full">
 
                   <!-- Cover Picture Upload -->
+                  <!-- <div class="w-full mt-3">
+                    <label class="block mb-1 text-gray-700 font-sans"> Select Cover Picture
+                      <span aria-hidden="true" class="text-red-600" title="This field is optional"></span>
+                    </label>
+                    <div class="flex rounded-lg border border-gray-300 shadow-sm w-full h-12">
+                      <label for="profile_picture"
+                        class=" img-inputblock w-[120px] pt-2 h-12 px-4 py-2 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded-lg cursor-pointer focus:outline-none img-input absolute">
+                        Choose file
+                      </label>
+                      <input id="cover_picture" type="file" @change="handleFileChange" accept="image/jpeg, image/png"
+                        class="w-full block px-5 py-3 h-12 border-0 focus:border-lightAzure focus:ring focus:ring-lightPastalBlue focus:ring-opacity-50 disabled:opacity-50 disabled:bg-gray-50 disabled:cursor-not-allowed rounded-lg">
+                    </div>
+                    <span v-if="fileError" class="text-red-500">{{ fileError }}</span>
+                     Show validation error -->
+                  <!--  </div> -->
+
+                  <!-- cover view -->
+                   
+                  <!-- <div class="w-full flex justify-center rounded-lg mt-3" v-if="profile_picture_preview">
+                    <img :src="profile_picture_preview" alt="Profile Picture Preview"
+                      class="w-32 h-32 object-cover border border-gray-300 rounded-lg" />
+
+                  </div>
+                  <div class="w-full flex justify-center rounded-lg mt-3">
+                    <button class="p-2 rounded-lg bg-red-600 mx-auto m-2 text-white"><svg
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      </svg>
+                    </button>
+                  </div> -->
+
                   <div class="w-full mt-3">
                     <label class="block mb-1 text-gray-700 font-sans"> Select Cover Picture
                       <span aria-hidden="true" class="text-red-600" title="This field is optional"></span>
                     </label>
                     <div class="flex rounded-lg border border-gray-300 shadow-sm w-full">
+
+                      <label for="profile_picture"
+                        class=" absolute img-inputblock w-[110px] pt-[13px] h-12 px-4 py-2 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded-lg cursor-pointer focus:outline-none img-input">
+                        Choose file
+                      </label>
                       <input id="cover_picture" type="file" @change="handleFileChange" accept="image/jpeg, image/png"
-                        class="w-full block px-5 py-3 border-0 focus:border-lightAzure focus:ring focus:ring-lightPastalBlue focus:ring-opacity-50 disabled:opacity-50 disabled:bg-gray-50 disabled:cursor-not-allowed rounded-lg">
+                        class="lock pt-1 text-black h-12 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg">
                     </div>
                     <span v-if="fileError" class="text-red-500">{{ fileError }}</span>
                     <!-- Show validation error -->
--- src/components/profiles/businessUserProfile/modals/nameModal.vue
+++ src/components/profiles/businessUserProfile/modals/nameModal.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Name change modal -->
-    <div v-if="visible" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div v-if="visible" class="relative z-index-220" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -59,15 +59,26 @@
                                     </div>
 
                                     <!-- Profile Picture Preview -->
-                                    <div class="w-full mt-3" v-if="profile_picture_preview">
+                                    <div class="w-full flex justify-center rounded-lg mt-3"
+                                        v-if="profile_picture_preview">
                                         <img :src="profile_picture_preview" alt="Profile Picture Preview"
-                                            class="w-32 h-32 object-cover border border-gray-300" />
+                                            class="w-32 h-32 object-cover border border-gray-300 rounded-lg" />
+
                                     </div>
-                                    
+                                    <div class="w-full flex justify-center rounded-lg mt-3">
+                                        <button class="p-2 rounded-lg bg-red-600 mx-auto m-2 text-white"><svg
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                        </button>
+                                    </div>
+
 
                                     <!-- Profile Picture Upload -->
-                                    <div class="w-full mt-3">
-                                        <label class="block mb-1 text-gray-700 font-sans">Profile Picture
+                                    <!-- <div class="w-full mt-3"> -->
+                                    <!-- <label class="block mb-1 text-gray-700 font-sans">Profile Picture
                                             <span aria-hidden="true" class="text-red-600"
                                                 title="This field is optional"></span>
                                         </label>
@@ -77,7 +88,26 @@
                                                 class="w-full block px-5 py-3 border-0 focus:border-lightAzure focus:ring focus:ring-lightPastalBlue focus:ring-opacity-50 disabled:opacity-50 disabled:bg-gray-50 disabled:cursor-not-allowed rounded-lg">
                                         </div>
                                         <span v-if="fileError" class="text-sm text-red-600 dark:text-red-500">{{
-        fileError }}</span>
+                                            fileError }}</span> -->
+                                    <!-- Show validation error -->
+                                    <!-- </div> -->
+
+                                    <div class="w-full mt-3">
+                                        <label class="block mb-1 text-gray-700 font-sans"> Profile Picture
+                                            <span aria-hidden="true" class="text-red-600"
+                                                title="This field is optional"></span>
+                                        </label>
+                                        <div class="flex rounded-lg border border-gray-300 shadow-sm w-full">
+
+                                            <label for="profile_picture"
+                                                class=" absolute img-inputblock w-[110px] pt-[13px] h-12 px-4 py-2 text-sm font-medium text-black bg-gray-50 border border-gray-300 rounded-lg cursor-pointer focus:outline-none img-input">
+                                                Choose file
+                                            </label>
+                                            <input id="cover_picture" type="file" @change="handleFileChange"
+                                                accept="image/jpeg, image/png"
+                                                class="lock pt-1 text-black h-12 w-full border-0 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 rounded-lg">
+                                        </div>
+                                        <span v-if="fileError" class="text-red-500">{{ fileError }}</span>
                                         <!-- Show validation error -->
                                     </div>
 
@@ -87,13 +117,19 @@
                     </div>
 
                     <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                        <button type="button" @click="saveName"  :disabled="loading"
+                        <button type="button" @click="saveName" :disabled="loading"
                             class="inline-flex w-full justify-center rounded-md bg-steelBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto">Save
                             changes
-                            <svg v-if="loading" aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
-                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
-                              </svg>
+                            <svg v-if="loading" aria-hidden="true" role="status"
+                                class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="#E5E7EB" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentColor" />
+                            </svg>
                         </button>
                         <button type="button" @click="$emit('close', 'name')"
                             class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
@@ -208,7 +244,7 @@ const saveProfilePicture = async () => {
         }
     } catch (error) {
         console.error('Error uploading profile picture', error);
-    }finally{
+    } finally {
         loading.value = false;
     }
 };
