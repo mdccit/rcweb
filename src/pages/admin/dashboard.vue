29eefaa54b8fc7087621045123d98a146013c0cb
Mewan Ayomal <mewan.ayomal@qualitapps.com>
2024-10-24 12:07:47 +0530

    #137000 admin manage modals and school profile ui fixes

--- src/pages/admin/dashboard.vue
+++ src/pages/admin/dashboard.vue
@@ -1,12 +1,87 @@
 <template>
-  <div class="container mx-auto mt-10">
-    <h1 class="text-2xl text-black mb-4">Dashboard</h1>
-  
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+        <h1 class="mt-8 text-6xl mb-8 font-bold">
+          Welcome!
+        </h1>
+        <p class="text-gray-700 leading-relaxed">
+          You're logged in as an admin. This is the admin dashboard. You can manage your application from here.
+        </p>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8">
+          <div class="p-6 border border-border rounded-lg">
+            <h3 class="text-lg font-semibold mb-2">
+              Company Resources</h3>
+            <p>To see these you need to be logged into your @recruited.pro Google Workspace account.
+              <b>
+                Please do not use @gmail.com accounts for business purposes.
+              </b>
+            </p>
+            <ul class="list-disc list-inside">
+              <li>
+                <a href="https://docs.google.com/document/d/1d5PpzRerGT3kHNmuX-OeRuXqFPN3jzTQSrZwkQVFUCA/edit?usp=drive_link"
+                  class="text-primary-500 hover:underline">
+                  [Doc] Company handbook
+                </a>
+              </li>
+              <li>
+                <a href="https://drive.google.com/drive/folders/18vUxFSKUwlmSyMTZ-UQG4Oqd6x3rWWAj?usp=drive_link"
+                  class="text-primary-500 hover:underline">
+                  Resources for everyone
+                </a>
+              </li>
+              <li>
+                <a href="https://drive.google.com/drive/folders/1NE1jwYNCckNy40uzYrVutCFHW-VBunh6?usp=drive_link"
+                  class="text-primary-500 hover:underline">Marketing resources</a>
+              </li>
+            </ul>
+          </div>
+          <div class="p-6 border border-border rounded-lg">
+            <h3 class="text-lg font-semibold mb-2">Be Careful with Emails</h3>
+            <p>Do not send emails to people who have opted out. Do not change email settings without the user's prior
+              permission.
+              <b> Do not send marketing emails more than ONCE A MONTH using this panel.
+
+              </b>
+            </p>
+            <p class="mt-4">
+              Emails may not be delivered to users. Log in to Zeptomail using Bitwarden credentials to check why.
+            </p>
+          </div>
+          <div class="p-6 border border-border rounded-lg">
+            <h3 class="text-lg font-semibold mb-2">Security Reminder
+
+            </h3>
+            <p>Store passwords and 2FA tokens in Bitwarden.
+              <b>
+                Make sure each password is as long as possible and IS NOT MEMORABLE. YOU SHOULD ONLY REMEMBER YOUR
+                BITWARDEN (and, optionally, Google Workspace) PASSWORD.)
+              </b>
+              Do not share sensitive credentials with anyone, even if they ask you to, in any message format.
+            </p>
+          </div>
+          <div class="p-6 border border-border rounded-lg">
+            <h3 class="text-lg font-semibold mb-2 text-red-500">
+              Legal Reminder
+            </h3>
+            <p>
+              Regularly check the legal@recruited.pro email address for copyright takedown, abuse and moderation
+              requests. To see shared inboxes, go to
+              <a href="//groups.google.com"><b>groups.google.com
+
+                </b>
+              </a>
+              and log in with your FirstName@recruited.pro address.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="my-16">
+
+      </div>
+    </div>
   </div>
-  <!-- <div>
-    <h1>Image Upload with Crop and Resize</h1>
-    <ImageUpload />
-  </div> -->
+
 </template>
 
 <script setup>
@@ -17,10 +92,10 @@ import { useUserStore } from '~/stores/userStore';
 const userStore = useUserStore()
 
 definePageMeta({
-    ssr: false,
-    layout: 'admin',
-    middleware: ['role'],
-    requiredRole: ['admin'],
+  ssr: false,
+  layout: 'admin',
+  middleware: ['role'],
+  requiredRole: ['admin'],
 });
 
 </script>
--- src/pages/business/businessMembers.vue
+++ src/pages/business/businessMembers.vue
@@ -3,8 +3,7 @@
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <div class="flex w-full justify-between gap-8">
                 <div class="flex items-center gap-4">
-                    <NuxtLink to="/admin/business"
-                       >
+                    <NuxtLink to="/admin/business">
                         <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                             viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                             stroke-linecap="round" stroke-linejoin="round">
@@ -13,22 +12,21 @@
                     </NuxtLink>
                     <h2 class="font-bold text-lg self-center text-black"> Editing: {{ name }} </h2>
                 </div>
+                <NuxtLink :to="`/app/profile/business/${slug}`">
+                    <button type="submit"
+                        class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-700 border-gray-300 focus:border-primary-300 focus:ring-primary-200">
+                        View
+                        <svg class="w-5 h-5 -mr-1 inline" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M17 7l-10 10"></path>
+                            <path d="M8 7l9 0l0 9"></path>
+                        </svg>
+                    </button>
+                </NuxtLink>
             </div>
-            <NuxtLink :to="`/app/profile/business/${slug}`">
-            <button type="submit"
-              class="border rounded-full shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-white hover:bg-gray-100 active:bg-gray-200 text-gray-700 border-gray-300 focus:border-primary-300 focus:ring-primary-200">
-              View
-              <svg class="w-5 h-5 -mr-1 inline" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path d="M17 7l-10 10"></path>
-                <path d="M8 7l9 0l0 9"></path>
-              </svg>
-            </button>
-          </NuxtLink>
         </div>
     </header>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <!-- Use the BusinessNavigation component -->
@@ -73,6 +71,62 @@
             </div>
         </div>
     </div>
+
+
+    <!-- Member Modal -->
+    <div class="w-full sm:max-w-md md:max-w-xl lg:max-w-2xl">
+        <div id="headlessui-dialog-panel-17" data-headlessui-state="open" unmount="true" dusk="modal-dialog">
+            <div class="bg-white p-8 md:p-12 rounded-xl relative">
+                <div class="absolute top-0 right-0 pt-8 pr-8 md:pt-12 md:pr-12"><button dusk="close-modal-button"
+                        type="button" class="text-gray-400 hover:text-gray-500"><span class="sr-only">Close</span><svg
+                            class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 6l-12 12"></path>
+                            <path d="M6 6l12 12"></path>
+                        </svg></button></div>
+                <h1 class="font-bold text-2xl mb-3">Updating QATest_Player01</h1>
+                <form data-splade-id="26t16Mg38oWsOaQI" method="PUT"
+                    action="https://qa1.recruited.qualitapps.com/admin/businesses/9c7d0c22-c388-4383-8da0-4d83319cf4ba/users/9c08427b-b28b-41ed-8f60-ce7bc26f201c">
+                    <fieldset>
+                        <div class=""><label class="block"><span class="block mb-1 text-gray-700 font-sans"> Role name
+                                </span>
+                                <div class="flex rounded-lg border border-gray-300 shadow-sm"><input
+                                        class="block px-5 py-3 w-full border-0 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:opacity-50 disabled:bg-gray-50 disabled:cursor-not-allowed rounded-lg"
+                                        name="role" type="text" data-validation-key="role"></div>
+                            </label><!----></div>
+                        <div class="my-8"></div>
+                        <div class="w-full"><label class="block"><span class="block mb-1 text-gray-700 font-sans">
+                                    Account type </span>
+                                <div class="relative">
+                                    <div class=""><select name="is_admin" data-validation-key="is_admin"
+                                            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-300 px-5 py-3 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:opacity-50">
+                                            <option value="1"> Admin (can manage users and posts) </option>
+                                            <option value="0"> Member (can post) </option>
+                                        </select></div><!---->
+                                </div>
+                            </label><!----></div>
+                        <div class="my-8"></div>
+                        <div class="self-center">
+                            <button type="submit"
+                                class="border mr-2 rounded-full text-white shadow-sm font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-steelBlue hover:bg-blue-600">
+                                <div class="flex flex-row items-center justify-center"><!----><span class=""> Save
+                                    </span></div>
+                            </button>
+                            <button type="submit"
+                                class="border rounded-full shadow-sm text-white font-bold py-2.5 px-8 focus:outline-none focus:ring focus:ring-opacity-50 bg-red-500 hover:bg-blue-600">
+                                <div class="flex flex-row items-center justify-center"><!----><span class=""> Remove Member
+                                    </span></div>
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
 <script setup>
@@ -106,7 +160,7 @@ const fetchBusinessMembers = async (businessId) => {
     try {
         const data = await $adminService.get_business_members(businessId);
         members.value = data.data || []; // Set the fetched members
-        slug.value =data.business_info.slug;
+        slug.value = data.business_info.slug;
     } catch (error) {
         console.error('Failed to load business members:', error.message);
         errors.value.push('Failed to load business members.');
@@ -132,14 +186,14 @@ const triggerNotification = (message, type) => {
 };
 
 const fetchBusinessDetails = async () => {
-  try {
-    const data = await $adminService.get_business_details(business_id.value);
-    name.value = data.business_info.name;
-    slug.value = data.business_info.slug;
+    try {
+        const data = await $adminService.get_business_details(business_id.value);
+        name.value = data.business_info.name;
+        slug.value = data.business_info.slug;
 
-  } catch (error) {
-    console.error('Error fetching business details:', error.message);
-  }
+    } catch (error) {
+        console.error('Error fetching business details:', error.message);
+    }
 };
 
 
