e182027882bedfb71e4927ec348679ae7dfcb686
Madubha Amunugama <madubha.amunugama@qualitapps.com>
2024-10-24 15:38:57 +0530

    #137616 UI navbar profile menu item

--- src/components/user/navbar.vue
+++ src/components/user/navbar.vue
@@ -205,6 +205,10 @@
                                 </li>
                             </ul>
                             <div class="py-2">
+                                <NuxtLink to="" class="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                    Profile</NuxtLink>
+                            </div>
+                            <div class="py-2">
                                 <NuxtLink to="/user/user-setting" @click.stop
                                     class="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                                     Settings</NuxtLink>
