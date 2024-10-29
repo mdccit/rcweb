f441fb89b0760141c1f2ee1696f648bc81958617
Isuru Sandalanka <isuru.sandalanka@qualitapps.com>
2024-10-29 10:12:13 +0530

    #137754 Nothing happens when click on the favorite button

--- src/components/profiles/businessUserProfile/businessUserCover.vue
+++ src/components/profiles/businessUserProfile/businessUserCover.vue
@@ -56,14 +56,14 @@
                         <div>
                             <div class="flex justify-end pr-[20px]">
                                 <div class="text-right ml-30px">
-                                    <button class="bg-lighterGray rounded-full w-[35px] h-[35px] p-0 m-1">
+                                    <!-- <button class="bg-lighterGray rounded-full w-[35px] h-[35px] p-0 m-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor"
                                             class="size-5 text-blue-500 m-auto">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                                         </svg>
-                                    </button>
+                                    </button> -->
                                 </div>
                     
                             </div>
